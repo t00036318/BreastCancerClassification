aea8e01a180139f651c97682916a581dbcd759f4
Vanessa Esther Pacheco De La Rosa <t00036318@utbvirtual.edu.co>
2016-11-23 00:43:17 -0500

    Almost done

--- SVM.py
+++ SVM.py
@@ -18,16 +18,9 @@ y.replace('B', 1, inplace=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.5, random_state=0)         #len(X_train) = 284, len(y_train) = 284
 X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=0)    #len(X_test) = 143, len(y_test) = 143, len(X_val) = 142, len(y_val) = 142
 
-#SUPPORT VECTOR MACHINE
-
-linear_svc = SVC(kernel='linear')                           #Kernel lineal
-poly_svc = SVC(kernel='poly', gamma=0.2, degree=2)          #Kernel polinomial
-rbf_svc = SVC(kernel='rbf', gamma=0.0001, C=7.0)            #Kernel gaussiano
-sig_svc = SVC(kernel='sigmoid', gamma=0.0000001, C=10.0)    #Kernel sigmoide
-kernels = [linear_svc, poly_svc, rbf_svc, sig_svc]
 
 
-def plot_confusion_matrix(confmat):                         #Funcion que grafica la matriz de confusion
+def plot_confusion_matrix(confmat):                         #Función que grafica la matriz de confusión
     fig, ax = plt.subplots(figsize=(2.5, 2.5))
     ax.matshow(confmat, cmap=plt.cm.Blues,  alpha=0.3)
     for i in range(confmat.shape[0]):
@@ -38,22 +31,30 @@ def plot_confusion_matrix(confmat):                         #Funcion que grafica
     plt.ylabel('Valores reales')
     plt.show()
 
+#SUPPORT VECTOR MACHINE
 
-def train(estimator, x_train, y_train):                     #Funcion que entrena el modelo
+linear_svc = SVC(kernel='linear')                           #Kernel lineal
+poly_svc = SVC(kernel='poly', gamma=0.2, degree=2)          #Kernel polinomial
+rbf_svc = SVC(kernel='rbf', gamma=0.0001, C=7.0)            #Kernel gaussiano
+sig_svc = SVC(kernel='sigmoid', gamma=0.0000001, C=10.0)    #Kernel sigmoide
+kernels = [linear_svc, poly_svc, rbf_svc, sig_svc]
+
+
+def train(estimator, x_train, y_train):                     #Función que entrena el modelo
     estimator.fit(x_train, y_train)
 
 
-def kFold(estimator, x_val, y_val):
+def kFold(estimator, x_val, y_val):                         #Validación KFold
     f = 5
     cv = KFold(len(y_val), f, shuffle=True, random_state=0)
     scores = cross_val_score(estimator, x_val, y_val, cv=cv)
     return scores
 
 
-def validate_svm(x_val, y_val, x_train, y_train, x_test, y_test):                                  #Funcion de validacion
+def validate_svm(x_val, y_val, x_train, y_train, x_test, y_test):    #Función de validación de SVM
     mayor, nombre, best_K = 0, "", 0
     for k in kernels:
-        scores = kFold(k, x_val, y_val)                      #Validacion KFold
+        scores = kFold(k, x_val, y_val)
 
         if k == linear_svc:
             K = "Lineal"
@@ -71,7 +72,7 @@ def validate_svm(x_val, y_val, x_train, y_train, x_test, y_test):               
             nombre = K
             best_K = k
 
-        print("Coeficiente de determinacion promedio del kernel", K,"=", prom)    #Coeficiente: calidad del modelo para replicar los resultados
+        print("Coeficiente de determinación promedio del kernel", K,"=", prom)    #Coeficiente: calidad del modelo para replicar los resultados
 
     print("Mejor kernel:", nombre)
     test(best_K, x_train, y_train, x_test, y_test)
@@ -80,18 +81,24 @@ def validate_svm(x_val, y_val, x_train, y_train, x_test, y_test):               
 def test(best_clf, x_train, y_train, x_test, y_test):
     train(best_clf, x_train, y_train)
     preds = best_clf.predict(x_test)
-    print_results(best_clf, x_test, y_test, preds)
+    print_results(y_test, preds)
 
 
-def print_results(estimator, x_test, y_test, preds):
-    print("Reporte de Clasificacion:")
-    print(metrics.classification_report(y_test, preds))
-    print("Matriz de Confusion:")
+def print_results(y_test, preds):
+    print("Matriz de Confusión:")
     M = metrics.confusion_matrix(y_test, preds)
     print(M)
     plot_confusion_matrix(M)
-    scores = cross_val_score(estimator, x_test, y_test, cv=5)
-    print("Exactitud: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    TP, FN, FP, TN = M[0][0], M[0][1], M[1][0], M[1][1]
+    accuracy = (TP + TN) / (TP + FN + FP + TN)
+    sensitivity = TP / (TP + FN)
+    specificity = TN / (FP + TN)
+    f1 = (2*TP)/(2*TP + FP + FN)
+    print("Exactitud =", accuracy)
+    print("Sensibilidad =", sensitivity)
+    print("Especificidad =", specificity)
+    print("Valor-F =", f1)
+
 
     #plot_decision_regions(X_train_plot, y_train, classifier=best_K)
     #plt.xlabel('petal length [standardized]')
@@ -108,7 +115,7 @@ print("-------------------------------------------------------------------------
 def validate_lr(x_val, y_val, x_train, y_train, x_test, y_test):
     mayor, c, p = 0, 0, ""
     for C in enumerate((100, 1, 0.01)):
-        clf_l1 = LogisticRegression(C=C, penalty='l1', tol=0.01)       #Valores mAs pequenios de c incrementan la regularizacion
+        clf_l1 = LogisticRegression(C=C, penalty='l1', tol=0.01)       #Valores más pequeños de c incrementan la regularización
         clf_l2 = LogisticRegression(C=C, penalty='l2', tol=0.01)
         train(clf_l1, x_train, y_train)
         train(clf_l2, x_train, y_train)
@@ -153,13 +160,8 @@ def validate_mlp(x_train, y_train, x_test, y_test, x_val, y_val):
                     A = act
                     HL = h
 
-            print("Mejores parametros: \n Solver =", S, "\n Activation = ", A, "\n Hidden Layer Sizes = ", HL)
+            print("Mejores parámetros: \n Optimizador=", S, "\n Función de Activación = ", A, "\n Tamaño de la capa oculta = ", HL)
             clf = MLPClassifier(solver=S, alpha=1e-5, hidden_layer_sizes=HL, activation=A, random_state=1)
             test(clf, x_train, y_train, x_test, y_test)
 
 validate_mlp(X_train, y_train, X_test, y_test, X_val, y_val)
-
-
-
-
-
